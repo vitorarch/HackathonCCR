626b62ca0ada34fe8b4741a96f44b449e60d6b9b
Vitor Chaves <archanjovitor@gmail.com>
2021-01-15 01:54:52 -0300

    creating interfaces and setting the data specifications for migration

---
+++ API/Interfaces/User/Resume/IResume.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces.User.Resume
+{
+    public class IResume
+    {
+    }
+}
